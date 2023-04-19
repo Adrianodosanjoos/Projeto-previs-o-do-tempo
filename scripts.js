a97a647c2dd29dfcfc0e00fef6c610d62cd5a0f3
Adrianodosanjoos <adriano.luizysa@gmail.com>
2023-04-19 09:05:32 -0300

    alterçoes

--- scripts.js
+++ scripts.js
@@ -1,29 +1,27 @@
 
+const key = "e10b679863656c37ee97276726f6cd05"
 
-let key = "e10b679863656c37ee97276726f6cd05"
-
-function colocarDadosNaTela(dados){
-    console.log(dados)
-  document.querySelector(".cidade").innerHTML="tempo em" + dados.name
-  document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) +  "°C"
-  document.querySelector(".texto-previão").innerHTML = dados.weater[0].description
-  document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
-  document.querySelector(".img-previsao").src = 'https://openweathermap.org/img/wn/$(dados.weather[0]).icon.png'
+function colocarDadosNaTela(dados) {
+    console.log('dados', dados)
+    document.querySelector(".cidade").innerHTML = "tempo em" + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previão").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/$(dados.weather[0]).icon.png`
 }
 
-async function buscarCidade(cidade){
+async function buscarCidade(cidade) {
 
-    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
 
     colocarDadosNaTela(dados)
 }
 
 function cliqueinobotao() {
-    let cidade = document.querySelector(".input-cidade").valor
+    const cidade = document.querySelector(".input-cidade").value
 
     buscarCidade(cidade)
+
+
 }
-
-
-
 
